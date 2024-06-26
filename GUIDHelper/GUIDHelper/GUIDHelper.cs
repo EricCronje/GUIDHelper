c584d15eb04d1c973ca7a6d548d8f985912c4db6
Eric Cronje <eric.cronje@gmail.com>
2024-06-26 18:26:32 +0800

    Initial commit

---
+++ GUIDHelper/GUIDHelper/GUIDHelper.cs
@@ -0,0 +1,17 @@
+﻿// Ignore Spelling: Validator
+
+using System.Text.RegularExpressions;
+
+namespace GUIDHelper
+{
+    public partial class GUIDHelper
+    {
+        [GeneratedRegex("^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$")]
+        private static partial Regex GuidValidatorRegex();
+
+        public static bool ValidateWithRegex(string input)
+        {
+            return GuidValidatorRegex().IsMatch(input);
+        }
+    }
+}
---
+++ GuidHelperConsole/GuidHelperConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+TestGUIDWithRegex();
+
+static void TestGUIDWithRegex()
+{
+    Console.WriteLine("GUID Test:");
+    var guidString = Guid.NewGuid().ToString();
+    string nonGuidString = "<Invalid>-<0000000>-<NotOK>-12345566756767erert";
+    string messageFormat = $"{0} | guid is {1}, non Guid is {2}";
+    var GUIDString = GUIDHelper.GUIDHelper.ValidateWithRegex(guidString).ToString();
+    var NonGUIDString = GUIDHelper.GUIDHelper.ValidateWithRegex(nonGuidString).ToString();
+    Console.WriteLine($"Is GUID: {GUIDString} -- {guidString}");
+    Console.WriteLine($"Is GUID: {NonGUIDString} -- {nonGuidString}");
+    Console.ReadLine();
+}
+
