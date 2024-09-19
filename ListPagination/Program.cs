4afc226a894305f257dba18d931ac33233cc390a
Eric Cronje <eric.cronje@gmail.com>
2024-09-20 01:59:08 +0800

    Created unit tests

--- ListPagination/Program.cs
+++ ListPagination/Program.cs
@@ -1,29 +1,68 @@
-﻿Console.WriteLine("PageSize : ");
+﻿using System.Text;
+
+Console.WriteLine("PageSize : ");
 _ = UInt32.TryParse(Console.ReadLine(), out uint pagesize);
 Console.WriteLine("TotalItems : ");
 _ = UInt32.TryParse(Console.ReadLine(), out uint totalitems);
 Pagination.Pagination pagination = new(pagesize, totalitems);
 
-bool bTrue = true;
-while (bTrue) 
+PrintOptions(pagination);
+
+bool bContinue = true;
+while (bContinue)
 {
-    Console.WriteLine($"n, b or x");
-    string? input = Console.ReadLine();
-    bTrue = (input != "x");
-    if (input == "n" && !pagination.IsLastPage())
-        pagination.GetNextPage();
-    
-    if (input == "b" && !pagination.IsFirstPage())
-        pagination.GetPreviousPage(); 
-    
-    if ((!pagination.IsLastPage() || !pagination.IsFirstPage()) || (pagination.IsLastPage() == pagination.IsFirstPage()))
+    StringBuilder sbValidOptions = new();
+
+    for (int i = 1; i! <= pagination.TotalPages; i++)
     {
-        Console.WriteLine($"Current page: {pagination.GetPageNumberCaption()}");
-        Console.WriteLine($"First Item: {pagination.FirstItemNumber}");
-        Console.WriteLine($"Last Item: {pagination.GetLastItemOnThePage()}");
-        Console.WriteLine($"Last Page: {pagination.IsLastPage()}");
-        Console.WriteLine($"First Page: {pagination.IsFirstPage()}");
+        sbValidOptions.Append("|" + "p" + i);
     }
-}; 
+    sbValidOptions.Append("|n|b|f|l|x");
+
+    Console.WriteLine($"n, b, f, l, x or p<page>");
+    string? input = Console.ReadLine();
+    bContinue = (input != "x");
+
+    if (input != null)
+    {
+        if (sbValidOptions.ToString().Contains(input))
+        {
+
+            if (input[..1] == "p" && input.Length > 1)
+            {
+                _ = UInt32.TryParse(input.AsSpan(1, 1), out UInt32 page);
+                pagination.GoToPage(page);
+            }
+
+            if (input == "n" && !pagination.IsLastPage())
+                pagination.GetNextPage();
+
+            if (input == "b" && !pagination.IsFirstPage())
+                pagination.GetPreviousPage();
+
+            if (input == "f" && !pagination.IsFirstPage())
+                pagination.GoToFirstPage();
+
+            if (input == "l" && !pagination.IsLastPage())
+                pagination.GoToLastPage();
+
+            PrintOptions(pagination);
+        }
+        else
+        {
+            PrintOptions(pagination);
+            Console.WriteLine($"Invalid input - {input}");
+        }
+    }
+};
 
 pagination.Dispose();
+
+static void PrintOptions(Pagination.Pagination pagination)
+{
+    Console.WriteLine($"Current page: {pagination.GetPageNumberCaption()}");
+    Console.WriteLine($"First Item: {pagination.FirstItemNumber}");
+    Console.WriteLine($"Last Item: {pagination.GetLastItemOnThePage()}");
+    Console.WriteLine($"Last Page: {pagination.IsLastPage()}");
+    Console.WriteLine($"First Page: {pagination.IsFirstPage()}");
+}
--- ListPagination/getProjectPath.sh
+++ ListPagination/getProjectPath.sh
@@ -1,1 +1,1 @@
-dotnet build $1 | grep -i ">" | awk '{print $3}'| sed 's/C:/\/c/g'| sed 's/\\/\//g'
+dotnet build $1 | grep -i ">" | awk '{print $3}'| sed 's/C:/\/c/g'| sed 's/\\/\//g' | sed 's/[^/]\+$//g'
